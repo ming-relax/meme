988a0b6cf9babccdc160f50934939e7a23c0c97b
Ming HU <humings@gmail.com>
2016-04-10 19:34:46 +0800

    add textfield style

--- meme/MemeViewController.swift
+++ meme/MemeViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var memeImage: UIImageView!
     @IBOutlet weak var memeCamera: UIBarButtonItem!
-    
+
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topTextField: UITextField!
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         memeCamera.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
@@ -20,8 +23,30 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName: UIColor.blackColor(),
+            NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName: -3
+            
+        ]
+        topTextField.defaultTextAttributes = memeTextAttributes
+//        topTextField.backgroundColor = UIColor.clearColor()
+        topTextField.borderStyle = UITextBorderStyle.None
+        topTextField.textAlignment = NSTextAlignment.Center
+        topTextField.delegate = self
+
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = NSTextAlignment.Center
+        bottomTextField.delegate = self
     }
 
+    @IBAction func cancelMeme(sender: UIBarButtonItem) {
+        memeImage.image = nil
+    }
+    
+    
     @IBAction func selectAlbum(sender: UIBarButtonItem) {
         let imagePicker = UIImagePickerController()
         presentViewController(imagePicker, animated: false, completion: nil)
@@ -35,6 +60,8 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         imagePicker.delegate = self
         
     }
+    
+    // UIImagePickerController delegate methods
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         memeImage.image = info["UIImagePickerControllerOriginalImage"] as? UIImage
         memeImage.contentMode = UIViewContentMode.ScaleAspectFit
@@ -44,5 +71,20 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    //UITextField delegate methods
+    func textFieldDidBeginEditing(textField: UITextField) {
+//        textField.text = ""
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
 }
 
